88870f2762d293ddcd632f69c9d47c1ce04ac38c
Garrett Marking <garrettmarking@Garretts-MacBook-Pro.local>
2021-11-27 16:21:55 -0800

    built simple HTTP REST API server in go

---
+++ go-rest-api/internal/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-openapi/loads"
+    "github.com/go-openapi/runtime/middleware"
+    "github.com/gmarking/learning-go/go-rest-api/pkg/swagger/server/restapi"
+
+    "github.com/gmarking/learning-go/go-rest-api/pkg/swagger/server/restapi/operations"
+)
+
+func main() {
+	// Initialize swagger
+	swaggerSpec, err := loads.Analyzed(restapi.FlatSwaggerJSON, "")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	api := operations.NewHelloAPIAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+
+	defer func() {
+		if err := server.Shutdown(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	server.Port = 8080
+	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
+	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
+	api.GetGopherNameHandler = operations.GetGopherNameHandlerFunc(GetGopherByName)
+
+	if err := server.Serve(); err != nil {
+        log.Fatalln(err)
+    }
+}
+
+func Health(operations.CheckHealthParams) middleware.Responder {
+	return operations.NewCheckHealthOK().WithPayload("OK")
+}
+
+func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
+	return operations.NewCheckHealthOK().WithPayload("Hello " + user.User + "!")
+}
+
+func GetGopherByName(gopher operations.GetGopherNameParams) middleware.Responder {
+	var URL string
+	if gopher.Name != "" {
+		URL = "https://github.com/scraly/gophers/raw/main/" + gopher.Name + ".png"
+	} else {
+		URL = "https://github.com/scraly/gophers/raw/main/dr-who.png"
+	}
+
+	response, err := http.Get(URL)
+	if err != nil {
+		fmt.Println("Error")
+	}
+	return operations.NewGetGopherNameOK().WithPayload(response.Body)
+}
